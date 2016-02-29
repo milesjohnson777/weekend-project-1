1728afb2b16f154acf96eabdcefda04cddbd4984
Miles Johnson <milesjohnsonwashere@gmail.com>
2016-02-28 19:25:43 -0600

    primary function of this app works, still working on the extra challenges

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#calculator').on('submit', function(event){
         event.preventDefault();
         var values = {};
-        $.each($('#calculator').serializeArray(), function(i, feild){
+        $.each($('#calculator').serializeArray(), function(i, field){
             values[field.name] = field.value;
         });
         $('#calculator').find('input[type=text]').val('');
@@ -10,15 +10,16 @@ $(document).ready(function(){
         allSalaries();
     });
 });
+
 var entry = [];
 
 function allSalaries(){
     var expense = 0;
-    for(var i = 0; i < entry.length; i++){
+    for(i = 0; i < entry.length; i++){
         var person = entry[i];
         expense += parseInt(person.salary/12);
+        console.log(person);
     }
-
 
 function listEntries(){
     $('.container').append('<p></p>');
@@ -34,9 +35,6 @@ function removeEntry(){
     expense -= $(this).parseInt(person.salary/12);
     $(this).parent().remove();
 }
-
-
-
-
     $('.totals').text("Here is the total of everyone's monthly salaries: " + expense);
+    $('.container').on('click', '.delete', removeEntry);
 }
