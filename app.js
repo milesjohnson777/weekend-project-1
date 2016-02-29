b7e72b051c68b3c42bcbf40feaddf2d87601ea5a
Miles Johnson <milesjohnsonwashere@gmail.com>
2016-02-28 18:29:27 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('#calculator').on('submit', function(event){
+        event.preventDefault();
+        var values = {};
+        $.each($('#calculator').serializeArray(), function(i, feild){
+            values[field.name] = field.value;
+        });
+        $('#calculator').find('input[type=text]').val('');
+        entry.push(values);
+        allSalaries();
+    });
+});
+var entry = [];
+
+function allSalaries(){
+    var expense = 0;
+    for(var i = 0; i < entry.length; i++){
+        var person = entry[i];
+        expense += parseInt(person.salary/12);
+    }
+
+
+function listEntries(){
+    $('.container').append('<p></p>');
+    var $el = $('container').children().last();
+
+    $el.append('.container').text(person.name + ' ' + person.IDNumber + ' ' + person.jobtitle + ' ' +
+    '$' + person.salary + ' per year').append('<button class="delete">Delete Entry</button>');
+}
+
+listEntries()
+
+
+
+
+    $('.totals').text("Here is the total of everyone's monthly salaries: " + expense);
+}
